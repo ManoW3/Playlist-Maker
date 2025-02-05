fa3a2d5c202ddf196aea0bfc93ef12fbbbb81fd0
ManoW3 <emanuel.wertman@gmail.com>
2025-02-04 20:37:32 -0500

    finished the project
    Co-authored-by: atharvaru <atharvaru@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -1,16 +1,27 @@
 // Atharva and Mano
 // 1/29/25
 // Playlist
-// Extra:
+// Extra: Searching/Save to File
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
+#include <random>
 #include <iomanip>
+#include <fstream>
+#include <unistd.h>
 
 using namespace std;
 
+int convertTime(string songLength) {
+    int colonPos = songLength.find(':');                    // this function finds the colon
+    int minutes = stoi(songLength.substr(0, colonPos));     // goes from the beggining of the string until the colon and converts it to an int so it works with either x:xx or xx:xx time formats
+    int seconds = stoi(songLength.substr(colonPos + 1));    // same thing but does after the semicolon for the seconds
+    return minutes * 60 + seconds;  
+}
+
 int main() {
-    
+    srand(time(0));
     vector<vector<string>> songs = {
         {"Travis Scott", "FE!N", "3:06"},
         {"Travis Scott", "4X$", "3:06"},
@@ -58,14 +69,116 @@ int main() {
         {"Kendrick Lamar", "The Blacker the Berry", "5:29"},
         {"Kendrick Lamar", "Sing About Me, I'm Dying of Thirst", "12:03"}
     };
-    while(true) {
         for (vector<string> curSong : songs) {
             cout << left << setw(20) << curSong[0]          // this is a thing in the iomanip library that gives the thing a set width
                 << setw(50) << curSong[1]
                 << setw(10) << curSong[2] << endl;
         }
         
+
+
+        int userChoice; 
+while(true){
+    cout<<"Would you like to do?\n1. Add Songs \n2. Play \n3. Shuffle \n4. Print Playlist \n5. Search \n6. Save to file\n7. Exit" << endl;
+    cin >> userChoice;
+    cin.ignore();
+    switch(userChoice){
+        case 1: {
+            cout << "What is the name of the artist: ";
+            string artistName;
+            getline(cin, artistName);
+    
+            cout << "What is the name of the song: ";
+            string songName;
+            getline(cin, songName);
+    
+            cout << "What is the length of the song (x:xx): ";
+            string songLength;
+            getline(cin, songLength);
+    
+
+    songs.push_back({artistName, songName, songLength});
+    
+    cout<<"SONG ADDED"<<endl;
+    break;
+            }
+        case 2:{
+            string songChoice;
+            string songLength;
+            int time;
+            bool found = false;
+
+            cout << "What Song? (Enter full name spelled correctly)" << endl;
+            getline(cin, songChoice);
+            for (vector<string> song : songs) {
+                if (song[1] == songChoice) {
+                    songLength = song[2];
+                    found = true;
+                    break;  // Exit loop once the song is found
+                }
+            }
+            if (!found)
+                cout << "Invalid Song" << endl;
+            else {
+                time = convertTime(songLength);
+                cout << "Playing " << songChoice << " for " << time << " seconds..." << endl;
+                sleep(time);
+            }
+            break;
+        }
+        case 3:{
+            shuffle(songs.begin(), songs.end(), default_random_engine{});
+            cout << "Songs Shuffled!" << endl;
+            break;
+        }
+        case 4:{
+            // print playlist
+             for (vector<string> curSong : songs) {
+            cout << left << setw(20) << curSong[0]          // this is a thing in the iomanip library that gives the thing a set width
+                << setw(50) << curSong[1]
+                << setw(10) << curSong[2] << endl;   
+            }
+            break;
+        }
+
+        case 5:{
+
+            cout << "What is the name of the song: ";
+            string searchSong;
+            getline(cin, searchSong);
+
+            for (vector<string> curSong : songs) {
+
+            if (curSong[1] == searchSong) {
+                 cout << "Artist: " << curSong[0] << endl;
+                cout << "Song: " << curSong[1] << endl;
+                cout << "Length: " << curSong[2] << endl;
+                
+                 break;
+                }
+            }
+        }
+
+        case 6: {
+            ofstream WriteFile("playlist.txt");
+            for (vector<string> song : songs) {
+                WriteFile << song[0] << ", " << song[1] << ", " << song[2] << endl;
+            }
+            cout << "File Made" << endl;
+            break;
+        }
+
+        case 7: {
+            return 1;
+        }
+
+        default:{
+            cout<<"Oh nah"<<endl;
+        }
     }
+    }
+
 
     return 0;
 }
+
